ee94a50b48e117ef367b5fd07feb47deb428cc66
MaratSam <148679643+MaratSam@users.noreply.github.com>
2024-01-07 19:16:50 +1000

    1

--- report_generetor/report_generetor/settings.py
+++ report_generetor/report_generetor/settings.py
@@ -104,11 +104,11 @@ if 'RDS_DB_NAME' in environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': environ['RDS_DB_NAME'],
+            'USER': environ['RDS_USERNAME'],
+            'PASSWORD': environ['RDS_PASSWORD'],
+            'HOST': environ['RDS_HOSTNAME'],
+            'PORT': environ['RDS_PORT'],
         }
     }
 else:
